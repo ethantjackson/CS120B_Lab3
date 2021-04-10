b5d923907cc4405b883c03ad2850992ffa06ce0e
Ethan Jackson <ejack023@wch136-24.cs.ucr.edu>
2021-04-09 21:59:37 -0700

    updated part 2

--- source/main.c
+++ source/main.c
@@ -14,16 +14,24 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRD = 0x00; PORTD = 0xFF;
-	DDRB = 0xFE; PORTB = 0x01;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
 	
-	unsigned short weight = 0x00;
+	unsigned char fuelLvl;
+	unsigned char tmpC;
     /* Insert your solution below */
     while (1) {
-	weight = (PIND << 1) | (PINB & 0x01);
-	if (weight <= 5) PORTB = PINB & 0x01;
-	else if (weight < 70) PORTB = (PINB & 0x01) | 0x04;
-	else PORTB = (PINB & 0x01) | 0x02;
+	tmpC = 0x00;
+	fuelLvl = PINA & 0x0F;
+	if (fuelLvl <= 0) tmpC = 0x00;
+	else if (fuelLvl <= 2) tmpC = 0x20;
+	else if (fuelLvl <=4) tmpC = 0x30;
+	else if (fuelLvl <=6) tmpC = 0x38;
+	else if (fuelLvl <=9) tmpC = 0x3C;
+	else if (fuelLvl <=12) tmpC = 0x3E;
+	else tmpC = 0x3F;
+	if (fuelLvl <=4) tmpC = tmpC | 0x40;
+	PORTC = tmpC;
     }
     return 1;
 }
